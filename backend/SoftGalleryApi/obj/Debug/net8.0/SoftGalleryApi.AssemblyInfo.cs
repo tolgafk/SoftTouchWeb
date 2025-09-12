75614de93e09f02c65581b9a1dd9201fd1fa9ee4
Tolga ferhan küçük <220201009@uzem.education>
2025-09-12 13:16:05 +0300

    yenilendi

--- backend/SoftGalleryApi/obj/Debug/net8.0/SoftGalleryApi.AssemblyInfo.cs
+++ backend/SoftGalleryApi/obj/Debug/net8.0/SoftGalleryApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftGalleryApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0488cef1b78ecbca5157b74166cae9c44678936d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d31d81aa95c66b9776986ab31539426598331af")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftGalleryApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftGalleryApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
